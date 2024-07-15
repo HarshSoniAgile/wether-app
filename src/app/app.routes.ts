24e452abff7821395a2e95841e777a7b19f3ac5c
Harsh Soni <harsh.m@agileinfoways.com>
2024-07-15 15:39:44 +0530

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ForecastWeatherComponent } from './forecast-weather/forecast-weather.component';
+
 
 export const routes: Routes = [
     {
